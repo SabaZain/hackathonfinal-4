c0b5f055aa773afae8be2512230696c6383eea80
Saba Zain <159971290+SabaZain@users.noreply.github.com>
2025-01-15 19:52:16 +0500

    Update Topbar.tsx

--- src/app/components/Topbar.tsx
+++ src/app/components/Topbar.tsx
@@ -17,7 +17,7 @@ const Topbar = ({setShowCart}: TopbarProps) => {
     {/*Topbar*/}
   return (
     
-    <div className='bg-primary md:flex lg:flex justify-around align items-center w-full p-2 px-12 md:px-40 lg:px-40'>
+    <div className='bg-primary md:flex lg:flex justify-around align items-center w-full p-2 px-7 md:px-40 lg:px-40'>
         <div className='flex gap-2 md:gap-6 lg:gap-6'>
         <div className='flex justify-start align items-center gap-2'>
         <IoMailOutline className='text-[#F1F1F1]'/>
