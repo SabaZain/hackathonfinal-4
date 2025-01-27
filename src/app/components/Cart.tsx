724d5ee3e39a912413c1bd0610df1cb2e1d1834b
Saba Zain <saba_punjwani@yahoo.com>
2025-01-28 00:05:57 +0500

    work updated

--- src/app/components/Cart.tsx
+++ src/app/components/Cart.tsx
@@ -46,17 +46,17 @@ const Cart = ({ setShowCart}:CartProps) => {
 
     
     return(
-        <div className="w-full min-h-screen fixed top-0 z-20 overflow-y-scroll">
+        <div className="w-full min-h-screen fixed top-0 z-20 overflow-y-scroll mr-6">
             <div className="max-w-[400px] w-full min-h-full bg-gray-600 absolute right-0 top-0 p-6">
                 <RxCross1
-                className="absolute right-0 top-0 m-6 text-[24px] cursor-pointer"
+                className="absolute right-0 top-0 m-6 mx-10 text-[24px] cursor-pointer"
                 onClick={() => setShowCart(false)}
                 />
                 <h3 className="pt-6 text-lg font-bold uppercase">
                 Your Cart
                 </h3>
 
-                <div className="mt-6 space-y-2">
+                <div className="mt-6 space-y-2 px-5">
                  {products?.map((item:CartProductType) => (
                     <CartProduct
                     key={item.id}
@@ -69,15 +69,14 @@ const Cart = ({ setShowCart}:CartProps) => {
                  ))}
                 </div>
 
-                <div className="flex justify-between items-center font-medium text-xl py-4">
+                <div className="flex justify-between items-center font-medium text-xl py-4 px-5">
                     <p>Total:</p>
                     <p>${getTotal()}.00</p>
                 </div>
 
                 {totalAmount > 0 ? (
                 <Link href="/Checkout?totalAmount=${totalAmount}">
-                <button 
-                className="bg-black text-white text-center w-full rounded-3xl py-2
+                <button className="bg-black text-white text-center w-full rounded-3xl py-2 px-2
                 hover:bg-accent mb-4 mt-4"
                 onClick={handleCheckout}
                 >
@@ -85,7 +84,7 @@ const Cart = ({ setShowCart}:CartProps) => {
                 </button>
                 </Link>
             ) : (
-                <div className="text-center text-red-700 font-semibold">
+                <div className="text-center text-[20px] text-red-700 font-semibold">
                 Please select products to add in cart.
                 </div>
             )}
