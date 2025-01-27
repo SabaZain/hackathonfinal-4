5e0741006139bb3696dc4fad62bcf7ec1a195149
Saba Zain <saba_punjwani@yahoo.com>
2025-01-28 00:22:23 +0500

    work updated

--- src/app/components/Cart.tsx
+++ src/app/components/Cart.tsx
@@ -46,8 +46,8 @@ const Cart = ({ setShowCart}:CartProps) => {
 
     
     return(
-        <div className="w-full min-h-screen fixed top-0 z-20 overflow-y-scroll mr-6">
-            <div className="max-w-[400px] w-full min-h-full bg-gray-600 absolute right-0 top-0 p-6">
+        <div className="w-full min-h-screen fixed top-0 z-20 overflow-y-scroll">
+            <div className="max-w-[400px] w-full min-h-full bg-gray-600 absolute right-0 top-0 p-6 mr-10 md:mr-0 lg:mr-0">
                 <RxCross1
                 className="absolute right-0 top-0 m-6 mx-10 text-[24px] cursor-pointer"
                 onClick={() => setShowCart(false)}
